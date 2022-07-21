bc1824ba8edd968db6efdf1592f91a046943bd78
Isabelly Martins <isabellyksml@gmail.com>
2022-07-21 20:00:56 -0300

    exercícios 008 concluídos

---
+++ exercicio1/script.js
@@ -0,0 +1,4 @@
+let numeroPar = Number(prompt("Digite um número par"))
+console.log(numeroPar%2)
+
+//caso o usuário insira números pares, o resultado é sempre 0. Caso insira número ímpar, o resultado é sempre 1.
---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+const primeiraOp = 5>20 && 5<2;
+console.log(primeiraOp)
+
+const segundaOp = 5===5 || 5==="5";
+console.log(segundaOp)
+
+const terceiraOp = !20>50;
+console.log(terceiraOp)
+
+const quartaOp = !(20>50);
+console.log(terceiraOp)
